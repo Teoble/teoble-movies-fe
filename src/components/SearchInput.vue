7ca985660644c2714ee7676ff5fd6bfa7a105c26
Rafael da Silva Pereira <rafael.pereira.sjbv@gmail.com>
2022-03-24 06:48:46 -0300

    Add first version of search input

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,48 +1,43 @@
 <template>
-    <md-content>
-        <div class="search-wrapper">
-            <md-autocomplete
-                v-model="searchedMovie"
-                :md-options="suggestedMovies"
-                md-input-placeholder="E.g. Harry Potter"
-                md-dense>
-                <label>Search Movies</label>
-                <md-icon>search</md-icon>
-            </md-autocomplete>
-        </div>  
-    </md-content>
+  <div class="search-wrapper">
+    <md-autocomplete
+      v-model="searchedMovie"
+      :md-options="suggestedMovies"
+      md-input-placeholder="E.g. Harry Potter"
+      md-dense
+    >
+      <label>Search Movies</label>
+      <md-icon>search</md-icon>
+    </md-autocomplete>
+  </div>
 </template>
 
 <script lang="ts">
-    import Vue from 'vue'
+import Vue from 'vue'
 
-    export default Vue.extend({
-      name: "SearchInput",
-      data() {
-          return {
-              searchedMovie: "",
-              suggestedMovies: [
-                  "Harry Potter 1",
-                  "Harry Potter 2",
-                  "Harry Potter 3"
-              ]
-          }
-      }
-    })
+export default Vue.extend({
+  name: 'SearchInput',
+  data() {
+    return {
+      searchedMovie: '',
+      suggestedMovies: ['Harry Potter 1', 'Harry Potter 2', 'Harry Potter 3']
+    }
+  }
+})
 </script>
 
 <style scoped>
-.md-content { 
-    width: 100%;
+.md-content {
+  width: 100%;
 }
 
-.md-field{ 
-    padding-top: 10px;
-    max-width: 560px;
+.md-field {
+  padding-top: 10px;
+  max-width: 560px;
 }
 
 .search-wrapper {
-    display: flex;
-    justify-content: center;
+  display: flex;
+  justify-content: center;
 }
 </style>
