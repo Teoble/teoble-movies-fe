ebd6677c02c90f66945c28be07d569280183fe23
Rafael da Silva Pereira <rafael.pereira.sjbv@gmail.com>
2022-03-24 06:49:21 -0300

    Add Movie Card component

---
+++ src/components/MovieCard.vue
@@ -0,0 +1,81 @@
+<template>
+  <div
+    class="movie-box md-layout-item md-small-size-50 md-xsmall-size-100 md-medium-size-33 md-size-20"
+    @mouseenter="showDetails = !showDetails"
+    @mouseleave="showDetails = !showDetails"
+  >
+    <div class="movie-poster">
+      <img :src="movie.poster" :alt="movie.title" />
+    </div>
+    <transition name="fade">
+      <div class="movie-details-wrapper" v-if="showDetails">
+        <div class="movie-details">
+          <span class="md-title">{{ movie.title }}</span>
+          <span class="md-subhead">{{ movie.year }}</span>
+        </div>
+        <div class="favorite-wrapper">
+          <md-button class="md-icon-button">
+            <md-icon v-if="movie.favorite">favorite</md-icon>
+            <md-icon v-else>favorite_border</md-icon>
+          </md-button>
+        </div>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script lang="ts">
+import IMovieCard from '@/model/MovieCard'
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class MovieCard extends Vue {
+  @Prop()
+  movie!: IMovieCard
+
+  showDetails: boolean = false
+}
+</script>
+
+<style lang="scss" scoped>
+.movie-box {
+  margin-top: 20px;
+  position: relative;
+  padding: 0;
+  cursor: pointer;
+
+  img {
+    height: 450px;
+  }
+
+  .movie-poster {
+    position: relative;
+  }
+
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 0.5s;
+  }
+  .fade-enter, .fade-leave-to /* .fade-leave-active em versões anteriores a 2.1.8 */ {
+    opacity: 0;
+  }
+
+  .movie-details-wrapper {
+    background: rgba(0, 0, 0, 0.7);
+    display: flex;
+    justify-content: space-between;
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    z-index: 2;
+    min-height: 93px;
+    margin: 0 20px;
+    padding: 5px 10px;
+
+    .movie-details {
+      display: grid;
+    }
+  }
+}
+</style>
---
+++ src/model/MovieCard.d.ts
@@ -0,0 +1,7 @@
+export default interface IMovieCard {
+  imdbID: string
+  title: string
+  year: number
+  poster: string
+  favorite: boolean
+}
